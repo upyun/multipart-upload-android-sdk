ff1f27cbdbeb9d84246488ddbac4c12f13510f02
xinglong.wang <xinglong.wang@upai.com>
2014-12-09 11:23:41 +0800

    change MAX_BLOCK_SIZE to 1000*1024

--- src/com/upyun/block/api/common/Config.java
+++ src/com/upyun/block/api/common/Config.java
@@ -4,7 +4,7 @@ import java.util.Calendar;
 
 public class Config {
 	//块最小1M
-	public static final int MIN_BLOCK_SIZE = 100 * 1024; 
+	public static final int MIN_BLOCK_SIZE = 1000 * 1024; 
 	
 	//请求地址
 	public static final String HOST = "http://m0.api.upyun.com/";
@@ -15,5 +15,5 @@ public class Config {
 	public static long expiration = Calendar.getInstance().getTimeInMillis() + 60;
 	
 	//客户分块大小
-	public static int blockSize = 100 * 1024;
+	public static int blockSize = 1000 * 1024;
 }
